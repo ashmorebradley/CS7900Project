aba1c85df33a9334ae1f52b1bb3fb01aa91586cc
maneeshgunt <101675430+maneeshgunt@users.noreply.github.com>
2022-04-19 13:48:13 -0400

    Author recovery code changes

--- AuthorRecoverer.py
+++ AuthorRecoverer.py
@@ -1,48 +1,114 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 15 21:46:49 2022
+import pandas as pd
+import reverse_geocoder as rg
+from datetime import datetime
 
-@author: ashmo
-"""
+#read data from the csv file
+data = pd.read_csv('tweetData.csv')
 
-#########################################
-#AuthorRecoverer Agent
-#########################################
-userDataRaw = {}
-#userData  is a username-indexed dictionary. Each user name points to a list of tuples containing content, topic, time, and a list of hashtags)
-
-userDataProcessed = {}
-#userData  is a username-indexed dictionary. Each user name points to a list of statistics that can be used to decide what user created a deleted tweet.
-#Contents of this diciton ary are TBD.
-
-#########################################
-#Variables used by the agent
-#########################################
-
-#########################################
-#Function used to add data to the agent.
-#User - A string.
-#Content - A string
-#Hashtag - A string
-#locaiton - A tuple of lat/lon values, floats
-#time - A string or date time object
-#topic - A string
+#storing the required data to a variable
+geo_Locations = data['geo']
+tweet_data = data['tweet']
+hashtag_data=data['hashtags']
+datetime_data=data['created_at']
+date_data=data['date']
 
 
-def loadData(user, content = 0, hashtag = 0, location = 0, time = 0, topic = 0):
-    #This function loads data into the userData dictionary.
-    #This function needs to ensure that it does not duplicate existing users.
+location_actual=[]
+
+def reverseGeocode(coordinates):
+    result = rg.search(coordinates)
+    return result
+
+def validateIncomingDate(date):
+    incomingDate=date.split(' ')
+    date_matched_data = []
+    for idx, s in enumerate(date_data):
+        if incomingDate[0] in s:
+            date_matched_data.append(idx)
+    return date_matched_data
  
-    return "" #This will need to be removed once the function is complete.
 
-def processRawData():
-    #This funciton process data in the userDataRaw variable into a usable form and stores the result in userDataProcessed
     
+def validateHashTagData(hashtag):
+    hashTag_matched_data=[]
+    for idx, d in enumerate(hashtag_data):
+        if d is not None and len(d)>2:
+            d_list=d.split(",")
+            for s in d_list:
+                for tag in hashtag:
+                    if tag.upper() in s.upper():
+                        hashTag_matched_data.append(idx)
+        
+    return hashTag_matched_data
     
-    return "" #This will need to be removed once the function is complete.
 
 def determineAuthor(Content, hashtag, location, time):
-    #This fucntion utilizes the data in userDataProcessed and returns the most-likely author.
-    author = ""
+    content_matched_data=[]
+    date_list=[]
+    hashTag_data=[]
     
-    return author
+    if Content is not None and Content != "":
+        for idx, s in enumerate(tweet_data):
+            if Content in s:
+                content_matched_data.append((idx))
+    
+    if time is not None and time != "":
+        date_list = validateIncomingDate(time)
+    
+    if hashtag is not None and hashtag != "" and len(hashtag) > 0:
+        hashTag_data=validateHashTagData(hashtag)
+        
+    print(content_matched_data)
+    print(date_list)
+    print(hashTag_data)
+        
+    common_in_all=set(content_matched_data) & set(date_list) & set(hashTag_data)
+    
+    print(common_in_all)
+    
+    if len(common_in_all) == 1:
+        return data.iloc[[list(common_in_all).pop()]].get('username')
+    elif len(content_matched_data) == 1:
+        return data.iloc[[content_matched_data[0]]].get('username')
+    elif len(date_list) == 1:
+        return data.iloc[[date_list[0]]].get('username')
+    elif len(hashTag_data) == 1:
+        return data.iloc[[hashTag_data[0]]].get('username')
+    elif location is not None:
+        list_of_indexes=[]
+        for s in content_matched_data:
+            list_of_indexes.append(s)
+        for s in date_list:
+            list_of_indexes.append(s)
+        for s in hashTag_data:
+            list_of_indexes.append(s)
+        res = []
+        index_with_geo=[]
+        for i in list_of_indexes:
+            if i not in res:
+                res.append(geo_Locations[i])
+                index_with_geo.append((i,geo_Locations[i]))
+                
+        dist_res = []
+        for i in res:
+            if i not in dist_res:
+                dist_res.append(i)
+        result = []
+        for t in dist_res:
+            list_lat_long=t.split(",")
+            coordinates =(list_lat_long[0],list_lat_long[1])
+            address=reverseGeocode(coordinates)
+            if address is not None and len(address)>0:
+                result.append(address[0].get('name'))
+        print(result)
+        lat_long_of_input=""
+        for idx, loc in enumerate(result):
+            if loc is not None and location is not None and location.upper() == loc.upper():
+                lat_long_of_input=dist_res[idx]
+        location_data=[]
+        for l in index_with_geo:
+            if lat_long_of_input == l[1]:
+                location_data.append(l[0])
+        
+        common_in_all_four=common_in_all & set(location_data)
+        return data.iloc[[list(common_in_all_four).pop()]].get('username') 
