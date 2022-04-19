73dcf61ec273fde3407e10499642b5e13b25806d
maneeshgunt <101675430+maneeshgunt@users.noreply.github.com>
2022-04-19 14:04:41 -0400

    Adding comments for the author retreival

--- AuthorRecoverer.py
+++ AuthorRecoverer.py
@@ -12,13 +12,12 @@ hashtag_data=data['hashtags']
 datetime_data=data['created_at']
 date_data=data['date']
 
-
-location_actual=[]
-
+#determine location based on latitude and longitude
 def reverseGeocode(coordinates):
     result = rg.search(coordinates)
     return result
 
+#retreive all indexes that have same date as incoming date in the data sheet
 def validateIncomingDate(date):
     incomingDate=date.split(' ')
     date_matched_data = []
@@ -28,7 +27,7 @@ def validateIncomingDate(date):
     return date_matched_data
  
 
-    
+#retreive all hash tags that contain incoming hashtags in the data sheet    
 def validateHashTagData(hashtag):
     hashTag_matched_data=[]
     for idx, d in enumerate(hashtag_data):
@@ -38,10 +37,49 @@ def validateHashTagData(hashtag):
                 for tag in hashtag:
                     if tag.upper() in s.upper():
                         hashTag_matched_data.append(idx)
-        
     return hashTag_matched_data
-    
 
+#Since we need an extra call to know the exact location i tried to drop the complete csv file data to the unique indexs in content, time & hashtag
+def getLocationData(content_matched_data,date_list,hashTag_data,location):
+    list_of_indexes=[]
+    for s in content_matched_data:
+        list_of_indexes.append(s)
+    for s in date_list:
+        list_of_indexes.append(s)
+    for s in hashTag_data:
+        list_of_indexes.append(s)
+    res = []
+    index_with_geo=[]
+    for i in list_of_indexes:
+        if i not in res:
+            res.append(geo_Locations[i])
+            index_with_geo.append((i,geo_Locations[i]))
+
+    dist_res = []
+    for i in res:
+        if i not in dist_res:
+            dist_res.append(i)
+    result = []
+    for t in dist_res:
+        list_lat_long=t.split(",")
+        coordinates =(list_lat_long[0],list_lat_long[1])
+        address=reverseGeocode(coordinates)
+        if address is not None and len(address)>0:
+            result.append(address[0].get('name'))
+    print(result)
+    lat_long_of_input=""
+    for idx, loc in enumerate(result):
+        if loc is not None and location is not None and location.upper() == loc.upper():
+            lat_long_of_input=dist_res[idx]
+    location_data=[]
+    for l in index_with_geo:
+        if lat_long_of_input == l[1]:
+            location_data.append(l[0])
+    return location_data;
+        
+    
+#determine author based on Content, hashtag, location & time
+#Eg: determineAuthor("Watching",["movie"],"Los Angeles","2022-03-28")
 def determineAuthor(Content, hashtag, location, time):
     content_matched_data=[]
     date_list=[]
@@ -75,40 +113,6 @@ def determineAuthor(Content, hashtag, location, time):
     elif len(hashTag_data) == 1:
         return data.iloc[[hashTag_data[0]]].get('username')
     elif location is not None:
-        list_of_indexes=[]
-        for s in content_matched_data:
-            list_of_indexes.append(s)
-        for s in date_list:
-            list_of_indexes.append(s)
-        for s in hashTag_data:
-            list_of_indexes.append(s)
-        res = []
-        index_with_geo=[]
-        for i in list_of_indexes:
-            if i not in res:
-                res.append(geo_Locations[i])
-                index_with_geo.append((i,geo_Locations[i]))
-                
-        dist_res = []
-        for i in res:
-            if i not in dist_res:
-                dist_res.append(i)
-        result = []
-        for t in dist_res:
-            list_lat_long=t.split(",")
-            coordinates =(list_lat_long[0],list_lat_long[1])
-            address=reverseGeocode(coordinates)
-            if address is not None and len(address)>0:
-                result.append(address[0].get('name'))
-        print(result)
-        lat_long_of_input=""
-        for idx, loc in enumerate(result):
-            if loc is not None and location is not None and location.upper() == loc.upper():
-                lat_long_of_input=dist_res[idx]
-        location_data=[]
-        for l in index_with_geo:
-            if lat_long_of_input == l[1]:
-                location_data.append(l[0])
-        
+        location_data=getLocationData(content_matched_data,date_list,hashTag_data,location)
         common_in_all_four=common_in_all & set(location_data)
         return data.iloc[[list(common_in_all_four).pop()]].get('username') 
